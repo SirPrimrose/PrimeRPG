eef1090bee8dfdb148f575223ca3229038ee7d31
SirPrimrose <Primmtech@gmail.com>
2021-03-14 20:21:26 -0500

    Delete log files older than 7 days

--- logging_handler.py
+++ logging_handler.py
@@ -1,20 +1,29 @@
 import datetime
 import logging
-from os import mkdir
+import os
+import time
 
 from consts import log_folder
 
+logfile_date_format = "%y_%m_%d"
+days_to_keep_logs = 7
+
 
 def setup_logging():
+    clean_log_folder()
     if not log_folder.exists():
-        mkdir(log_folder)
-    time = datetime.datetime.utcnow()
+        os.mkdir(log_folder)
+    current_time = datetime.datetime.utcnow()
     counter = 0
-    filename = "db_log_{}_{}.log".format(time.strftime("%d_%m_%y"), counter)
+    filename = "db_log_{}_{}.log".format(
+        current_time.strftime(logfile_date_format), counter
+    )
     logfile_path = log_folder / filename
     while logfile_path.exists():
         counter += 1
-        filename = "db_log_{}_{}.log".format(time.strftime("%d_%m_%y"), counter)
+        filename = "db_log_{}_{}.log".format(
+            current_time.strftime(logfile_date_format), counter
+        )
         logfile_path = log_folder / filename
     init_db_logger(logfile_path)
 
@@ -27,3 +36,14 @@ def init_db_logger(logfile: str):
     fh.setLevel(logging.DEBUG)
     fh.setFormatter(formatter)
     log.addHandler(fh)
+
+
+def clean_log_folder():
+    now = time.time()
+
+    for f in os.listdir(log_folder):
+        f = log_folder / f
+        if os.stat(f).st_mtime < now - days_to_keep_logs * 86400:
+            if os.path.isfile(f):
+                print("Removed old log file: {}".format(f.name))
+                os.remove(f)
